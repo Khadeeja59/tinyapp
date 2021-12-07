e42d7a4f9ebac9e748c69eac4d13f00f8b2ad2ee
Khadeeja59 <kkausar59@gmail.com>
2021-12-07 03:01:55 +0000

    Filling out url_index.ejs template

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+//Added ejs template engine
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,6 +11,12 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// Adding a new routeHandler for /urls and use res.render() to pass the URL data to our template. 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
