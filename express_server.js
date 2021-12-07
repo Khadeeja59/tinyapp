a496205fe1a2905bcad8a8f895875d915cba484f
Khadeeja59 <kkausar59@gmail.com>
2021-12-07 20:48:59 +0000

    Deleting urls

--- express_server.js
+++ express_server.js
@@ -11,7 +11,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//Generate a Random ShortURL
+//-----------------------------------Generate a Random ShortURL---------------------------------------------------------
 function generateRandomString(n) {
   let randomString = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//Adding other routes. 
+//-------------------------------------Adding other routes.-------------------------------------------------------------- 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; //value in template variable should be in obj form.
   res.render("urls_index", templateVars);
@@ -47,11 +47,11 @@ app.get("/hello", (req, res) => {
 // });
 
 
-// Adding a GET Route to Show the Form
+//-----------------------------Adding a GET Route to Show the Form-----------------------------------------------------------------
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-//Adding a POST Route to Receive the Form Submission
+//----------------------Adding a POST Route to Receive the Form Submission---------------------------------------
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
   // res.send("Ok");  // Respond with 'Ok' (we will replace this)
@@ -64,7 +64,7 @@ app.post("/urls", (req, res) => {
   // res.status(200).send();
   
 });
-// Redirect after form submission
+//-----------------------Redirect after form submission-----------------------------------------------------------
 app.get("/u/:shortURL", (req, res) => {
   // const longURL = ...
   const shortURL = req.params.shortURL;
@@ -75,6 +75,21 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//-----------------------------------Deleting URLs----------------------------------------------------------------------
+app.post('/urls/:shortURL/delete', (req, res) => {
+
+  console.log("DELETE HERE");
+
+  // extract the id from the url
+  // req.params
+  const shortURL = req.params.shortURL;
+  // delete it from the db
+  delete urlDatabase[shortURL];
+
+  // redirect to /quotes
+  res.redirect('/urls');
+
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
