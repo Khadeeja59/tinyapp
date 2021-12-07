9d859e33692a749488bca2a5b7eb5778bed7c0d7
Khadeeja59 <kkausar59@gmail.com>
2021-12-07 20:04:19 +0000

    Saving the longURL and shortURL on urlDatabase

--- express_server.js
+++ express_server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-
-//4. Body parser for POST request(using this as the data in body is sent as buffer)
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = 8080; // default port 8080
-//Added ejs template engine
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -14,7 +11,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//6. Generate a Random ShortURL
+//Generate a Random ShortURL
 function generateRandomString(n) {
   let randomString = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -25,40 +22,61 @@ function generateRandomString(n) {
  return randomString;
 }
 
+const shortURL = generateRandomString(6);
 
 
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// // 1. Adding a new routeHandler for /urls and use res.render() to pass the URL data to our template. 
-// app.get("/urls", (req, res) => {
-//   const templateVars = { urls: urlDatabase }; //value in template variable should be in obj form.
-//   res.render("urls_index", templateVars);
-// });
-
-// // 2.Adding a new routeHandler for /urls:shortURL, :=>shortURL is a route parameter , its value will be available in req.params obj. 
+//Adding other routes. 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; //value in template variable should be in obj form.
+  res.render("urls_index", templateVars);
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+//Adding a new routeHandler for /urls:shortURL
 // app.get("/urls/:shortURL", (req, res) => {
-//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]/* What goes here? */ }; //value in template variable should be in obj form.
+//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; //value in template variable should be in obj form.
 //   res.render("urls_show", templateVars);
 // });
-// 3. Adding a GET Route to Show the Form
+
+
+// Adding a GET Route to Show the Form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-// 5. Adding a POST Route to Receive the Form Submission
+//Adding a POST Route to Receive the Form Submission
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // console.log(req.body);  // Log the POST request body to the console
+  // res.send("Ok");  // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString(6);
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+  // res.send(shortURL);       
+  res.status(200).redirect(`/u/${shortURL}`); ///shorter version for our redirect links: /u/:shortURL
+  // res.status(200).send();
+  
 });
+
+// Redirect after form submission
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  console.log(longURL);
+  // const templateVars = {shortURL,longURL}
+  // res.render("urls_show", templateVars);
+  res.redirect(longURL);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
